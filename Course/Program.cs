27871a2cad68996f3e21a335975ef22c1367dc46
Cleiber Rodrigo <cleiberrodrigox@hotmail.com>
2023-03-30 16:49:35 -0300

    Add project files.

---
+++ Course/Program.cs
@@ -0,0 +1,25 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = Entities.Enums.OrderStatus.PendingPayment
+            };
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(order);
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
